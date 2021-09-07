4e90f6000b5a97e481d7fae2e5ec40720511497c
Kaspars Skels <kaspars.skels@gmail.com>
2021-09-07 11:43:07 -0500

    Fix naming for run-pipeline.sh
    Signed-off-by: Kaspars Skels <kaspars.skels@gmail.com>

--- tekton/run-pipeline.sh
+++ tekton/run-pipeline.sh
@@ -1,5 +1,5 @@
 tkn pipeline start build-and-deploy-spring-app \
-    -w name=shared-workspace,volumeClaimTemplateFile=03_persistent_volume_claim.yaml \
+    -w name=shared-workspace,volumeClaimTemplateFile=persistent_volume_claim.yaml \
     -w name=maven-settings,secret="maven-settings" \
     -p deployment-name=spring-app \
     -p git-url=https://github.com/kskels/spring-app.git\
