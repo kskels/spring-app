3094dff657c3734d34fd72bc35c8a36b15708c14
Kaspars Skels <kaspars.skels@gmail.com>
2021-09-07 09:22:24 -0500

    Update Tekton pipelines with new deploy layout
    Signed-off-by: Kaspars Skels <kaspars.skels@gmail.com>

--- tekton/run-pipeline.sh
+++ tekton/run-pipeline.sh
@@ -2,5 +2,5 @@ tkn pipeline start build-and-deploy-spring-app \
     -w name=shared-workspace,volumeClaimTemplateFile=03_persistent_volume_claim.yaml \
     -w name=maven-settings,secret="maven-settings" \
     -p deployment-name=spring-app \
-    -p git-url=https://gitea.apps.ocp4.kskels.com/demos/spring-app.git \
+    -p git-url=https://github.com/kskels/spring-app.git\
     -p IMAGE=docker.apps.ocp4.kskels.com/spring-app
