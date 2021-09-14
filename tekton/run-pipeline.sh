ce1ff2f86c19fc1888651eca7ca8b6e0ec72d9df
Kaspars Skels <kaspars.skels@gmail.com>
2021-09-14 14:02:55 -0500

    Update tekton examples to be more self-sufficient
    Signed-off-by: Kaspars Skels <kaspars.skels@gmail.com>

--- tekton/run-pipeline.sh
+++ tekton/run-pipeline.sh
@@ -3,4 +3,4 @@ tkn pipeline start build-and-deploy-spring-app \
     -w name=maven-settings,secret="maven-settings" \
     -p deployment-name=spring-app \
     -p git-url=https://github.com/kskels/spring-app.git\
-    -p IMAGE=docker.apps.ocp4.kskels.com/spring-app
+    -p IMAGE=image-registry.openshift-image-registry.svc:5000/spring-apps-cicd/spring-app
