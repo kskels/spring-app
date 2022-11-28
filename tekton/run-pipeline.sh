724ff1985d9cc852fbe1951589695883d7a9307a
Kaspars Skels <kaspars.skels@gmail.com>
2022-11-28 09:03:57 -0600

    Minor fixes
    Signed-off-by: Kaspars Skels <kaspars.skels@gmail.com>

--- tekton/run-pipeline.sh
+++ tekton/run-pipeline.sh
@@ -2,5 +2,6 @@ tkn pipeline start build-and-deploy-spring-app \
     -w name=shared-workspace,volumeClaimTemplateFile=persistent_volume_claim.yaml \
     -w name=maven-settings,secret="maven-settings" \
     -p deployment-name=spring-app \
-    -p git-url=https://github.com/kskels/spring-app.git\
+    -p git-url=https://github.com/kskels/spring-app.git \
+    -p git-revision=main \
     -p IMAGE=image-registry.openshift-image-registry.svc:5000/spring-apps-cicd/spring-app
